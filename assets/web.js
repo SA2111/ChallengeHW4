646a7a7e7b03a2cc086e4f33fcc4c005e03b68ad
SA2111 <127169775+SA2111@users.noreply.github.com>
2023-07-21 10:47:15 -0400

    Update web.js

--- assets/web.js
+++ assets/web.js
@@ -1,6 +1,12 @@
 // variables
 // Api set up
-var api = "6247ab9cfce973852042385f8920ad4d"
+var button = document.querySelector('.enter')
+var value = document.querySelector('.value')
 
+var name = document.querySelector('.name')
+var description = document.querySelector('.description')
+var tempriture = document.querySelector('.tempriture')
+
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + 6247ab9cfce973852042385f8920ad4d;
 // call api
-api.openweathermap.org/data/2.5/weather?q={city name}&appid={6247ab9cfce973852042385f8920ad4d}
+fetch(queryURL)
